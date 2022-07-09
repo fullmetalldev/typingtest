b7d0d4f5373cc0a4bdb801381618ef4dfc97d08a
Erbol Nurekov <64440211+nrkvv04@users.noreply.github.com>
2022-07-09 17:12:46 +0600

    web application system and algorythms chaned

--- src/Components/Test/Test.js
+++ src/Components/Test/Test.js
@@ -5,11 +5,20 @@ import {useNavigate} from "react-router-dom";
 import "./test.css"
 
 const Test = () => {
+
     const [userText, setUserText] = useRecoilState(userTextSelect);
 
     const [inputText, setInputText] = useState('');
+    const [time, setTime] = useState(0);
 
+    const [all, setAll] = useState(0);
+    const [accuracy, setAccuracy] = useState(0);
 
+    const timerFunc = () => {
+        setInterval(()=>{
+            setTime(prev => prev + 0.1)
+        }, 100)
+    };
 
     const navigate = useNavigate();
     useEffect
@@ -25,16 +34,27 @@ const Test = () => {
         <div className="Test">
             <div className="container">
                 <div className="text">
-                    <h3>{userText}</h3>
                     <h3 className="userText">
                         {inputText}
+                        <span style={{color: "black"}}>{userText}</span>
                     </h3>
+                    <div className="statistic">
+                        <span>Скорость: {(all * (time / 60)).toFixed(2)}</span>
+                        <span>Точность: {all > 0 ? (100 - (accuracy * 100 / all)).toFixed(2) : 100.00}%</span>
+                    </div>
                 </div>
                 <input value={inputText} onChange={(e) => {
-                    if (e.target.value.split('')[e.target.value.length-1] === userText.split('')[e.target.value.length-1]) {
-                       setInputText(
-                           inputText + e.target.value.split('')[e.target.value.length-1]
-                       )
+                    setAll(all + 1);
+                    if (e.target.value.split('')[e.target.value.length - 1] === userText.split('')[0]) {
+                        setUserText(userText.slice(1));
+                        setInputText(
+                            inputText + e.target.value.split('')[e.target.value.length - 1]
+                        );
+                    } else {
+                        setAccuracy(accuracy + 1)
+                    }
+                    if (inputText.length === 1) {
+                        timerFunc()
                     }
                 }}/>
             </div>
